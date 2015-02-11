450e5f759ed9b626638317342a4f03f9764ed8f7
Ylenia Cilano <saylen@SAYLEN>
2015-02-11 17:33:09 +0100

    add jobnotify, dlrnotify and runrange to sendSMS

--- geodrop/SMSOut/SMSSend.php
+++ geodrop/SMSOut/SMSSend.php
@@ -38,6 +38,26 @@ class SMSSend extends GeodropRequest
    * @var date
    */
   private $deferred;
+  /**
+   * Url for job notification
+   * @var string
+   */
+  private $jobnotify;
+  /**
+   * Url for dlr notification
+   * @var string
+   */
+  private $dlrnotify;
+  /**
+   * Time from which messages are not sent (HH:MM)
+   * @var date
+   */
+  private $runrange_ltime;
+  /**
+   * Number of hours during which messages are not sent
+   * @var int
+   */
+  private $runrange_timewindow;
   
   /**
    * Creates a new <CODE>SMSSend</CODE> instance
@@ -48,10 +68,22 @@ class SMSSend extends GeodropRequest
    * @param string deferred (optional) Date and time in the format "Y-m-d H:i:s",
    * used to send the message to a certain date,
    * if not specified the message is sent immediately
-   *
+   * @param string jobnotify Url for job notification
+   * @param string dlrnotify Url for dlr notification
+   * @param string runrange_ltime Time from which messages are not sent (HH:MM)
+   * @param int runrange_timewindow Number of hours during which messages are not sent
+   * 
    * @throws Exception If parameters are not valid
    */
-  public function __construct($dest_msisdns,$message_text,$tpoa,$deferred=null) 
+  public function __construct(
+    $dest_msisdns,
+    $message_text,
+    $tpoa,
+    $deferred=null,
+    $jobnotify=null,
+    $dlrnotify=null,
+    $runrange_ltime=null,
+    $runrange_timewindow=null)
   {
     if(!isset($dest_msisdns,$message_text,$tpoa))
     {
@@ -67,6 +99,14 @@ class SMSSend extends GeodropRequest
     $this->set_tpoa($tpoa);
     if(isset($deferred))
       $this->set_deferred(date('Y-m-d H:i:s',strtotime(trim($deferred))));
+    if(isset($jobnotify))
+      $this->set_jobnotify($jobnotify);
+    if(isset($dlrnotify))
+      $this->set_dlrnotify($dlrnotify);
+    if(isset($runrange_ltime))
+      $this->set_runrange_ltime(date('H:i',strtotime(trim($runrange_ltime))));
+    if(isset($runrange_timewindow))
+      $this->set_runrange_timewindow($runrange_timewindow);
   }
   
   public function __destruct()
@@ -76,6 +116,10 @@ class SMSSend extends GeodropRequest
     unset($this->dest_msisdns);
     unset($this->tpoa);
     unset($this->deferred);
+    unset($this->jobnotify);
+    unset($this->dlrnotify);
+    unset($this->runrange_ltime);
+    unset($this->runrange_timewindow);
   }
 
   public function decodeResponse( $http_response )
@@ -101,13 +145,52 @@ class SMSSend extends GeodropRequest
     {
       foreach($this->dest_msisdns as &$msisdn)
       {
-	$this->body.= '<DEST msisdn="'.$msisdn.'" />';
+	      $this->body.= '<DEST msisdn="'.$msisdn.'" />';
       }
     }
     $this->body.= '</LIST>';
     $this->body.= '<TPOA>'.$this->tpoa.'</TPOA>'; 
     if(isset($this->deferred))
       $this->body.= '<DEFERRED>'.$this->deferred.'</DEFERRED>';
+    if(isset($this->jobnotify))
+    {
+      $this->body.= '<JOBNOTIFY>';
+      if(is_string($this->jobnotify))
+      {
+        $this->body.= '<NOTIFYURL>'.$this->jobnotify.'</NOTIFYURL>';
+      }
+      elseif(is_array($this->jobnotify))
+      {
+        foreach ($this->jobnotify as $jobnot) 
+        {
+          $this->body.= '<NOTIFYURL>'.$jobnot.'</NOTIFYURL>';
+        }
+      }
+      $this->body.= '</JOBNOTIFY>';
+    }
+    if(isset($this->dlrnotify))
+    {
+      $this->body.= '<DLRNOTIFY>';
+      if(is_string($this->dlrnotify))
+      {
+        $this->body.= '<NOTIFYURL>'.$this->dlrnotify.'</NOTIFYURL>';
+      }
+      elseif (is_array($this->dlrnotify))
+      {
+        foreach ($this->dlrnotify as $dlrnot) 
+        {
+          $this->body.= '<NOTIFYURL>'.$dlrnot.'</NOTIFYURL>';
+        }
+      }
+      $this->body.= '</DLRNOTIFY>';
+    }
+    if(isset($this->runrange_ltime) && isset($this->runrange_timewindow))
+    {
+      $this->body.= '<RUNRANGE>';
+      $this->body.= '<L_TIME>'.$this->runrange_ltime.'</L_TIME>';
+      $this->body.= '<TIME_WINDOW>'.$this->runrange_timewindow.'</TIME_WINDOW>';
+      $this->body.= '</RUNRANGE>';
+    }
     $this->body.= '</GEOSMS>';
   }
 
@@ -151,6 +234,50 @@ class SMSSend extends GeodropRequest
   public function get_deferred()
   {
      return $this->deferred;
+  }
+
+  /**
+   * Returns the url for job notification
+   * 
+   * @access public
+   * @return string
+   */
+  public function get_jobnotify()
+  {
+    return $this->jobnotify;
+  }
+
+  /**
+   * Returns the url for dlr notification
+   * 
+   * @access public
+   * @return string
+   */
+  public function get_dlrnotify()
+  {
+    return $this->dlrnotify;
+  }
+
+  /**
+   * Returns the time from which messages are not sent
+   * 
+   * @access public
+   * @return date
+   */
+  public function get_runrange_ltime()
+  {
+    return $this->runrange_ltime;
+  }
+
+  /**
+   * Returns the number of hours during which messages are not sent
+   * 
+   * @access public
+   * @return int
+   */
+  public function get_runrange_timewindow()
+  {
+    return $this->runrange_timewindow;
   }
   
   //setters
@@ -211,6 +338,50 @@ class SMSSend extends GeodropRequest
       }
     }
     $this->deferred = $deferred;
+  }
+
+  /**
+   * Sets the url for job notification
+   * 
+   * @param string jobnotify Url for job notification
+   * @return void
+   */
+  public function set_jobnotify($jobnotify)
+  {
+    $this->jobnotify = $jobnotify;
+  }
+
+  /**
+   * Sets the url for dlr notification
+   * 
+   * @param string jobnotify Url for dlr notification
+   * @return void
+   */
+  public function set_dlrnotify($dlrnotify)
+  {
+    $this->dlrnotify = $dlrnotify;
+  }
+
+  /**
+   * Sets the time from which messages are not sent (HH:MM)
+   * 
+   * @param date runrange_ltime Time from which messages are not sent (HH:MM)
+   * @return void
+   */
+  public function set_runrange_ltime($runrange_ltime)
+  {
+    $this->runrange_ltime = $runrange_ltime;
+  }
+
+  /**
+   * Sets the number of hours during which messages are not sent
+   * 
+   * @param int runrange_timewindow Number of hours during which messages are not sent
+   * @return void
+   */
+  public function set_runrange_timewindow($runrange_timewindow)
+  {
+    $this->runrange_timewindow = $runrange_timewindow;
   }
 }
 
